cf62c67ee11ce347ebe0f71d567753ddc52ebd5a
Lukas0610 <mail@lukasberger.at>
2017-05-20 14:12:57 +0200

    use different variable-names to prevent overwriting

--- lib/build.sh
+++ lib/build.sh
@@ -1,17 +1,17 @@
 #
 # Automated Android Build Script - Simple and automated Android Build-Script
 # Copyright (C) 2017  Lukas Berger
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
@@ -22,44 +22,44 @@ if [ ! $AABS -eq 1 ]; then
 fi
 
 function upload_build {
-	source_dir="${__rom_source}/out/target/product/${__codename}"
-	upload_path="${upload_basedir}/${__copy_path}"
-	output_artifcat="${source_dir}/$(basename ${source_dir}/${__output_expr})"
+	_source_dir="${__rom_source}/out/target/product/${__codename}"
+	_upload_path="${upload_basedir}/${__copy_path}"
+	_output_artifcat="${_source_dir}/$(basename ${_source_dir}/${__output_expr})"
 
 	# create SFTP batch file
-	batch_file=$(mktemp)
-	echo "mkdir $(dirname ${upload_path})" > $batch_file
-	echo "cd $(dirname ${upload_path})" >> $batch_file
-	echo "put ${output_artifcat} ${upload_path}" >> $batch_file
+	_batch_file=$(mktemp)
+	echo "mkdir $(dirname ${_upload_path})" > $_batch_file
+	echo "cd $(dirname ${_upload_path})" >> $_batch_file
+	echo "put ${_output_artifcat} $_{upload_path}" >> $_batch_file
 	echo "exit" >> $batch_file
 	__assert__ $?
 
 	# connect and upload
-	sshpass -p "${upload_pass}" sftp -P$upload_port -b $batch_file $upload_user@$upload_host
+	sshpass -p "${upload_pass}" sftp -P$upload_port -b $_batch_file $upload_user@$upload_host
 	__assert__ $?
 
 	# clean up
-	rm $batch_filev
+	rm $_batch_file
 	__assert__ $?
 }
 
 function copy_build {
-	source_dir="${__rom_source}/out/target/product/${__codename}"
-	copy_dir="${copy_basedir}/$(dirname ${__copy_path})"
-	copy_path="${copy_basedir}/${__copy_path}"
-	output_artifcat="${source_dir}/$(basename ${source_dir}/${__output_expr})"
+	_source_dir="${__rom_source}/out/target/product/${__codename}"
+	_copy_dir="${copy_basedir}/$(dirname ${__copy_path})"
+	_copy_path="${copy_basedir}/${__copy_path}"
+	_output_artifcat="${_source_dir}/$(basename ${_source_dir}/${__output_expr})"
 
-	mkdir -p "${copy_dir}"
+	mkdir -p "$_copy_dir}"
 	__assert__ $?
 
-	cp "${output_artifcat}" "${copy_path}"
+	cp "${_output_artifcat}" "${_copy_path}"
 	__assert__ $?
 }
 
 function start_build {
-	source_dir="${__rom_source}"
+	_source_dir="${__rom_source}"
 
-	cd $source_dir
+	cd $_source_dir
 	__assert__ $?
 
 	# prepare build
@@ -75,12 +75,12 @@ function start_build {
 	fi
 
 	# build
-	make otapackage -j${__concr_jobs}
+	make bacon -j${__concr_jobs}
 	__assert__ $?
 
 	# Build finished, copy/upload if enabled
 	# --------------------------------------
-	# I know it's not the correct way, but 
+	# I know it's not the correct way, but
 	# we can be sure the variable is empty or set
 	if [[ "${copy_basedir}" != "" ]]; then
 		copy_build
